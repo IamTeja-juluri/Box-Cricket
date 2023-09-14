95174a91d6948d8e4952ecc03b68aff92ecbfa08
Teja Juluri <julurisaiteja6789@gmail.com>
2023-09-14 19:05:26 +0530

    made an rds connection from mysql workbench

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@
 const { Sequelize } = require('sequelize');
 const ServerConfig = require('./server-config')
 
-const sequelize = new Sequelize('boxcricket', ServerConfig.DB_USER, ServerConfig.DB_PASSWORD, {
+const sequelize = new Sequelize(ServerConfig.DB_NAME, ServerConfig.DB_USER, ServerConfig.DB_PASSWORD, {
   host: ServerConfig.DB_HOST,
   dialect: 'mysql'
 });
